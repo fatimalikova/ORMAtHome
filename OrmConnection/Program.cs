52c057cd17c653b7e282428952b2d0609f41cf9e
fatimalikova <fatimakhm@code.edu.az>
2026-01-06 22:39:50 +0400

    Added program.cs

--- OrmConnection/Program.cs
+++ OrmConnection/Program.cs
@@ -20,16 +20,54 @@ namespace OrmConnection
             //Console.WriteLine("Student added successfully.");
 
             //Bulk insert
-            Student[] students = new Student[]
-            {
-                new Student() { Name = "Jane Smith", Description = "Another student", Age = 22 },
-                new Student() { Name = "Mike Johnson", Description = "Yet another student", Age = 23 },
-                new Student() { Name = "Emily Davis", Description = "More students", Age = 20 }
-            };
-            context.Students.AddRange(students);
-            context.SaveChanges();
-            Console.WriteLine("Bulk students added successfully.");
+            //Student[] students = new Student[]
+            //{
+            //    new Student() { Name = "Jane Smith", Description = "Another student", Age = 22 },
+            //    new Student() { Name = "Mike Johnson", Description = "Yet another student", Age = 23 },
+            //    new Student() { Name = "Emily Davis", Description = "More students", Age = 20 }
+            //};
+            //context.Students.AddRange(students);
+            //context.SaveChanges();
+            //Console.WriteLine("Bulk students added successfully.");
 
+            Group group = new Group()
+            {
+                Name = "Group A",
+                Limit = 30
+            };
+            context.Group.Add(group);
+            context.SaveChanges();
+            Console.WriteLine("Group added successfully.");
+            Group group1 = new Group()
+            {
+                Name = "Group B",
+                Limit = 25
+            };
+            context.Group.Add(group1);
+            context.SaveChanges();
+            Console.WriteLine("Group added successfully.");
+
+            Student student1 = new Student()
+            {
+                Name = "Alice Brown",
+                Description = "Student in Group A",
+                Age = 19,
+                GroupId = group.Id
+            };
+            context.Students.Add(student1);
+            context.SaveChanges();
+            Console.WriteLine("Student with group added successfully.");
+            AppDbContext context2 = new AppDbContext();
+            var studentsInGroupA = context2.Students
+                .Where(s => s.GroupId == group.Id)
+                .ToList();
+            Console.WriteLine("Students in Group A:");
+            foreach (var student in studentsInGroupA)
+            {
+                Console.WriteLine(student);
+
+
+            }
 
         }
     }
